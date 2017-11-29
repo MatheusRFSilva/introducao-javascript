a4b6cfbd629d7a08d5a9e8b4cf8ee8ff3bbed3f5
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2017-11-29 09:45:52 -0200

    Aula 3-1

--- js/principal.js
+++ js/principal.js
@@ -1,36 +1,46 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector('#primeiro-paciente');
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i< pacientes.length ;i++){
+	//console.log(pacientes[i]);
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+	var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector('.info-imc');
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >=1000 ){
+	var tdImc = paciente.querySelector('.info-imc');
 
-	console.log('Peso inválido');
-	pesoEhValido = false;
-	tdImc.textContent = "Peso invalido"
+	var pesoEhValido = true;
+	var alturaEhValida = true;
+
+	if (peso <= 0 || peso >=1000 ){
+
+		console.log('Peso inválido');
+		pesoEhValido = false;
+		tdImc.textContent = "Peso invalido"
+	}
+
+	if(altura <=0 || altura >= 3.00){
+		console.log('Altura inválido');
+		alturaEhValida = false;
+		tdImc.textContent = "Altura invalido"
+	}
+
+	if(alturaEhValida && pesoEhValido){
+		var imc = peso / (altura * altura) ;	
+		tdImc.textContent = imc.toFixed(2);
+	}
+
+
 }
 
-if(altura <=0 || altura >= 3.00){
-	console.log('Altura inválido');
-	alturaEhValida = false;
-	tdImc.textContent = "Altura invalido"
-}
 
-if(alturaEhValida && pesoEhValido){
-	var imc = peso / (altura * altura) ;	
-	tdImc.textContent = imc;
-}
 
 
 
