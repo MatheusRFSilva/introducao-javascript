05833caeed34fbd99df8f58528436eb6947224f5
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2017-12-03 01:20:33 -0200

    Aula 06-03 Validando o Restante dos Campos

--- js/form.js
+++ js/form.js
@@ -15,9 +15,10 @@ botaoAdicionar.addEventListener('click',function(event){
 
 var erros = validaPaciente(paciente);
 
+console.log(erros);
 if (erros.length > 0){
-		var mensagemErro = document.querySelector("#mensagem-erro");
-		mensagemErro.textContent = erros;
+		exibeMensagensDeErro(erros);
+
 		return
 }
 
@@ -26,9 +27,20 @@ if (erros.length > 0){
 
 	tabela.appendChild(pacienteTr);
 		form.reset();
+		var mensagensErro =  document.querySelector("#mensagens-erro");
+		mensagensErro.innerHTML = '';
 });
 
 
+function exibeMensagensDeErro(erros){
+	var ul = document.querySelector("#mensagens-erro");
+		ul.innerHTML ='';
+		erros.forEach(function(erro){
+			var li = document.createElement('li');
+			li.textContent = erro;
+			ul.appendChild(li);
+		});
+}
 function obtemPacienteDoFormulario(form){
 
 	var paciente = {
@@ -85,11 +97,20 @@ function montaTd(dado,classe){
 
 function validaPaciente(paciente){
 		var erros = [];
+
+		if(paciente.nome.length ==0) erros.push("O nome não pode ser em branco")
+
 		if (!validaPeso(paciente.peso))	erros.push('Peso é Invalido');
 
 		if(!validaAltura(paciente.altura)) {
 			erros.push("Altura é Iválido")
 		}
+
+		if(paciente.gordura.length == 0 ) erros.push("A gordura não pode ser em branco");
+
+		if (paciente.peso.length == 0) erros.push("O peso não pode ser em branco");
+
+		if(paciente.altura.length == 0) erros.push("A altura não pode ser em branco")
 
 		return erros;
 }
