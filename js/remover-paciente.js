fad47b8921fb45901789ea4f5354690bece7cb4f
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2017-12-29 09:36:32 -0200

    Aula 07 - 03 Delegando eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,20 @@
 var pacientes = document.querySelectorAll('.paciente');
 
+var tabela = document.querySelector("#tabela-pacientes")
+
+tabela.addEventListener('dblclick',function(event){
+
+  var alvoEvento = event.target;
+  var paiDoAlvo = alvoEvento.parentNode;
+
+  paiDoAlvo.remove();
+
+});
+/*
 pacientes.forEach(function(paciente){
     paciente.addEventListener('dblclick',function(){
       console.log("Fui clicado em duplo clic");
       this.remove();
     })
 });
+*/
