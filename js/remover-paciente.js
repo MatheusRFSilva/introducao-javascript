744e74c9df5e65e02c83244eed8aa5a9abc0adb8
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-01-29 17:16:51 -0200

    Aula 07 - 06 Animando a remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -7,7 +7,11 @@ tabela.addEventListener('dblclick',function(event){
   var alvoEvento = event.target;
   var paiDoAlvo = alvoEvento.parentNode;
 
+  paiDoAlvo.classList.add("fadeOut");
+  setTimeout(function(){
   paiDoAlvo.remove();
+},500);
+
 
 });
 /*
