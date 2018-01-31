5d1d538e8638b4915f7042d02c6c2f5f84e3e3e8
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-01-31 19:39:21 -0200

    Aula 09 - 05 Capturando erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,17 +6,28 @@ botaoAdicionar.addEventListener('click', function(){
     var xhr = new XMLHttpRequest();
 
     xhr.open('GET','https://api-pacientes.herokuapp.com/pacientes');
-
+    //xhr.open('GET','https://api-pacientes.herokuapp.com/paci1111entes');
     xhr.addEventListener('load',function(){
-      var resposta = xhr.responseText;
-      //console.log(resposta);
-      var pacientes = JSON.parse(resposta);
-      //console.log(pacientes);
+      var erroAjax = document.querySelector('#erro-ajax');
+      if(xhr.status == 200){
+        erroAjax.classList.add("invisivel");
+        var resposta = xhr.responseText;
+        //console.log(resposta);
+        var pacientes = JSON.parse(resposta);
+        //console.log(pacientes);
 
 
-      pacientes.forEach(function(paciente){
-        adicionaPacienteNaTabela(paciente);
-      })
+        pacientes.forEach(function(paciente){
+          adicionaPacienteNaTabela(paciente);
+        })
+
+      }else {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+
+        erroAjax.classList.remove("invisivel");
+      }
+
     });
     xhr.send();
 
